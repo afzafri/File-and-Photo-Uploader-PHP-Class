dfe5f2d7014f2b8d1ffad5fd8d7435866b697b37
Afif Zafri <zafri_21596@yahoo.com>
2016-10-27 00:15:05 +0800

    Add demo file

---
+++ demo.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Upload image demo</title>
+</head>
+<body>
+
+<form action="up.php" method="post" enctype="multipart/form-data">
+Upload: <input type="file" name="fileupload"><br>
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
+
+<?php
+
+if(isset($_POST['submit']))
+{
+	//include class
+	require_once('./uploader.class.php');
+
+	//create new object
+	$obj = new Uploader();
+
+	//set the needed values
+	$obj->dir = "./images/"; //directory to store the image/file
+	$obj->files = $_FILES["fileupload"]; //receive from form
+	$obj->filetype = array('png','gif'); //set the allowed image/file extensions
+	$obj->nameid = "reaperz"; //image/file name or id
+	$obj->size = 100000; //set file/image size limit. note: 100000 is 100KB
+	$obj->upimg = true; //if you want to upload image, set true, else set false.
+
+	//upload
+	$stat = $obj->upload();
+
+	//print upload result/status
+	echo $stat;
+}
+
+
+?>
