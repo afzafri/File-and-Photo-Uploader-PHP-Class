10f2cfd1bca74e9d2b6e7d2f5f71df865256268f
Afif Zafri <zafri_21596@yahoo.com>
2016-11-06 12:52:49 +0800

    update demo file

--- demo.php
+++ demo.php
@@ -28,8 +28,9 @@ if(isset($_POST['submit']))
 	$obj->filetype = array('png','gif'); //set the allowed image/file extensions
 	$obj->nameid = "reaperz"; //image/file name or id
 	$obj->size = 100000; //set file/image size limit. note: 100000 is 100KB
-	$obj->upimg = true; //if you want to upload image, set true, else set false.
-	$obj->exists = true; //set true if you want to check if the file already exist, if exist, file will not upload
+	//below is optional. If don't want to use it, remove from code. default for both is false.
+	$obj->upimg = true; //set true if want to upload image.
+	$obj->exists = true; //set true if want to check if the file already exist, if exist, file will not upload.
 
 	//upload
 	$stat = $obj->upload();
